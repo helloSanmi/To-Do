d860de82dfb6f079b2f4bec46d2eb5019d05e2b0
Oluwasanmi Idowu <95680393+helloSanmi@users.noreply.github.com>
2022-04-23 18:09:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var todo = document.getElementById("todo");
 var button = document.getElementById("add");
 var display = document.getElementById("display");
+let array = [];
 
 function addTodo() {
-  if (todo.value !== "") {
+  array = [...array, todo.value];
+  if (array.length > 0) {
     var input = document.createElement("li");
-    var textnode = document.createTextNode(todo.value);
+    var textnode = document.createTextNode(array);
     input.appendChild(textnode);
     input.innerHTML +=
       ' <button onclick="this.parentNode.remove()">Done</button>';
