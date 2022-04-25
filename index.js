79123407a5425d593075bd29f8cceb7c2990fd8f
Oluwasanmi Idowu <95680393+helloSanmi@users.noreply.github.com>
2022-04-25 11:55:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,17 +3,32 @@ var button = document.getElementById("add");
 var display = document.getElementById("display");
 let array = [];
 
+//function to take input and display output
 function addTodo() {
-  array = [...array, todo.value];
-  if (array.length > 0) {
+    array = [...array, todo.value];
     var input = document.createElement("li");
-    var textnode = document.createTextNode(array);
+    var textnode = document.createTextNode(array[array.length-1]);
     input.appendChild(textnode);
     input.innerHTML +=
-      ' <button onclick="this.parentNode.remove()">Done</button>';
+    ' <button onclick="this.parentNode.remove">Done</button>';
     display.appendChild(input);
+    console.log(array);
     todo.value = "";
+}
+
+//function for button click
+function afterClick(){
+  if (todo.value.length > 0) {
+    addTodo();
   }
 }
 
-button.addEventListener("click", addTodo);
+//function for Enter key
+function afterEnter(event){
+  if(todo.value.length > 0 && event.which === 13){
+    addTodo();
+  }
+}
+
+button.addEventListener("click", afterClick);
+todo.addEventListener("keypress", afterEnter);
