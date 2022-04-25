9f7bec9946540cf156d7f99e81b47bdcba699ba6
Oluwasanmi Idowu <95680393+helloSanmi@users.noreply.github.com>
2022-04-25 16:46:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,20 +5,15 @@ let array = [];
 
 
 function display_todo() {
+  array = [...array, todo.value]
   var input = document.createElement("li");
   var textnode = document.createTextNode(array[array.length-1]);
   input.appendChild(textnode);
   input.innerHTML += 
   `   <button onclick="this.parentNode.remove();">Delete</button><br>`;
   display.appendChild(input);
-  
   console.log(array);
   todo.value = "";
-}
-
-function add_todo() {
-  array = [...array, todo.value]
-  display_todo();
 }
 
 function delete_todo(arr, index) {
@@ -28,13 +23,13 @@ function delete_todo(arr, index) {
 
 function afterClick() {
     if (todo.value.length > 0) {
-      add_todo();
+      display_todo();
     }
   }
   
   function afterEnter(event) {
     if (todo.value.length > 0 && event.which === 13) {
-      add_todo();
+      display_todo();
     }
   }
 
